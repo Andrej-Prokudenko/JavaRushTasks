27b0c9cee3636d779972f7351a7b43959aa02968
Andrew Prokudenko <andrejprokudenko@mail.ru>
2021-04-13 11:31:24 +0300

    Initial commit

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task15.task1525;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Файл в статическом блоке
+*/
+
+public class Solution {
+    public static List<String> lines = new ArrayList<String>();
+
+    static {
+        String line;
+        try {
+            FileReader fr = new FileReader(Statics.FILE_NAME);
+            BufferedReader br = new BufferedReader(fr);
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+            br.close();
+            fr.close();
+        } catch (FileNotFoundException fN) {
+            fN.printStackTrace();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+
+
+    public static void main(String[] args) throws Exception{
+        /*FileInputStream reader = new FileInputStream(Statics.FILE_NAME);
+        Scanner scanner = new Scanner(reader);
+        while (scanner.hasNext()){
+            lines.add(scanner.nextLine());
+        }*/
+        System.out.println(lines);
+    }
+}
